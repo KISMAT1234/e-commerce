be3399a993eda4b2678757f12fbb26677248758a
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-09 19:28:38 +0545

    kismat

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,19 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
+import { FaSnapchatSquare } from "react-icons/fa";
 function Footer(){
     return(
         <>
-         <footer className="bg-gray-400 w-[100%] h-[60vh] px-5 py-5">
+         <footer className="bg-gray-400 w-[100%]  px-5 py-5">
             <div className="">
                 <div className="sm:flex">
 
-                     <input type="text" className="w-[100%] h-[5vh] text-2xl  " placeholder="Enter your email"/>
-                     <button className="bg-red-600 rounded sm:ml-10 w-[100%] sm:w-[30%] text-3xl h-[4vh] mt-2">Subscribe</button>
+                     <input type="text" className="w-[100%] h-[8vh] text-4xl  " placeholder="Enter your email"/>
+                     <button className="bg-red-600 rounded sm:ml-10 w-[100%] sm:w-[30%] text-3xl h-[vh] mt-2">Subscribe</button>
                 </div>
-                <div className="flex flex-wrap ">
+                <div className="flex flex-wrap justify-between mt-5">
                     <div className="">
                         <h1 className="font-bold text-xl">LOCATION</h1>
                         <h5>Copenhagen, Denmark</h5>
@@ -22,19 +27,29 @@ function Footer(){
                         <h1 className="font-bold text-xl">HELP</h1>
                         <h5>Payments</h5>
                         <h5>Shipping</h5>
-                        <h5>Cancellation</h5>
+                        <h5>Cancellation of order</h5>
                         <h5>Returns</h5>
                         <h5>Error page</h5>
                     </div>
 
                     <div className="">
                         <h1 className="font-bold text-xl">POLICY</h1>
-                        <h5></h5>
+                        <h5>Return Policy</h5>
+                        <h5>Terms Of Use</h5>
+                        <h5>Security</h5>
+                        <h5>Privacy</h5>
+                        <h5>Press</h5>
                     </div>
 
                     <div className="">
                     <h1 className="font-bold text-xl">FOLLLOW US</h1>
-
+                    <div className="flex justify-between">
+                        <FaFacebook/>
+                        <FaInstagram />
+                        <FaTwitterSquare />
+                        <FaTiktok />
+                        <FaSnapchatSquare />
+                    </div>
                         <h5></h5>
                     </div>
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setSearch } from "../redux/slices/SearchSlice";
+import kismatlogo from "../assets/kismatlogo.png"
 
 const Navbar = () => {
   const dispatch = useDispatch();
   return (
     <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10 border-red-600 border-y-4 ">
-      <div>
-        <h3 className="text-xl font-bold text-gray-600">
+      <div className="w-[15%]">
+        {/* <h3 className="text-xl font-bold text-gray-600">
           {new Date().toUTCString().slice(0, 16)}
         </h3>
-        <h1 className="text-2xl font-bold ">Kismat Foods</h1>
+        <h1 className="text-2xl font-bold ">Kismat Foods</h1> */}
+        <img src={kismatlogo} className=" lg:w-[40%]"/>
+      </div>
+      <div>
+        <h1 className="text-2xl md:text-3xl font-bold md:mt-3">Burgers! Because no great story started with salad </h1>
       </div>
       <div>
         <input
