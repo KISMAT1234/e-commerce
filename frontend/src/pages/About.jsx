c91e448a5e32ffb93b4f7dea8e5d866ebd2a6fb2
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-27 19:03:18 +0545

    about section photo added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,13 +1,37 @@
 import Navbar from "../components/Navbar";
 import Footer from  "../components/Footer";
+// import chief from "../assets/about/chief.jpg"
+import view from "../assets/about/view.jpg"
+// import from "../assets/about"
+// import from "../assets/about"
+// import from "../assets/about"
+// import from "../assets/about"
+// import from "../assets/about"
 
 
 function About() {
     return (
         <>
+        <div className="absolute">
+
+    
           <Navbar/>
-      
+        
+          <div className="relative">
+            <div className="flex justify-center">
+               <img src={view} className="w-[100%] mt-5"/>
+            </div>
+            <div className=" ">
+              <h1 className=" text-gray-600 text-center text-8xl ">Best View  in the World</h1>
+            </div>
+          </div>
+
+          <div>
+            
+          </div>
+
           <Footer/>
+          </div>
         </>
     )
 }
