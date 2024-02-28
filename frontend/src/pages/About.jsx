5506b95a3a5a1f037d9742904d4b878623b1093d
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-28 21:42:48 +0545

    chief and waiter added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,8 +2,8 @@ import Navbar from "../components/Navbar";
 import Footer from  "../components/Footer";
 // import chief from "../assets/about/chief.jpg"
 import view from "../assets/about/view.jpg"
-// import from "../assets/about"
-// import from "../assets/about"
+import chief from "../assets/about/chief.jpg"
+import waiter from "../assets/about/waiter.jpeg"
 // import from "../assets/about"
 // import from "../assets/about"
 // import from "../assets/about"
@@ -22,12 +22,20 @@ function About() {
                <img src={view} className="w-[100%] mt-5"/>
             </div>
             <div className=" ">
-              <h1 className=" text-gray-600 text-center text-8xl ">Best View  in the World</h1>
+              <h1 className="mt-20 text-gray-60pg0 text-center text-8xl ">Best View  in the World</h1>
             </div>
           </div>
 
-          <div>
-            
+           <hr></hr>
+          <h1 className="mt-20 mb-5 text-4xl text-center">We have well experienced chief and waiter</h1>
+          <div className="flex justify-around">
+             <div>
+                   <img src={chief} className="w-[500px]"/>
+             </div>
+             <div>
+             <img src={waiter} className="w-[400px] rounded border-rounded-xl"/>
+
+             </div>
           </div>
 
           <Footer/>
